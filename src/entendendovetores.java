fd3eef0f3ab2202282d0e811c0380d8b4efaf4b9
João Neiva <jneivasousa@gmail.com>
2021-03-05 20:13:38 -0300

    Entendendo Vetores

---
+++ src/entendendovetores.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+public class entendendovetores {
+	
+	//SE FOR NECESSÁRIO ACEITAR MAIS  OU MENOS ENTRADAS  BASTA ALTERAR 
+	public static final int TAMANHO = 10;
+	public static void main(String[] args) {
+		int vetor[]=new int [10];
+		
+		for(int i=0;i<10;i++) {
+		
+		vetor[0]=5;
+		vetor[1]=10;
+		}
+		
+		
+		System.out.println("A primeira posição contém o valor " + vetor[0]);
+		System.out.println("A primeira posição contém o valor " + vetor[1]);
+	}
+
+}
